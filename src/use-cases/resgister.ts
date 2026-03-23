a02a0c28aae62e826f9be09d648a21f5900b4fc3
Anderson Almeida <andersonpa10pa@gmail.com>
2026-03-23 17:12:22 -0300

    Caso de Uso e Design Patterns - Interface do repositório

--- src/use-cases/resgister.ts
+++ src/use-cases/resgister.ts
@@ -1,4 +1,4 @@
-import { prisma } from '@/lib/prisma'
+import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcrypt'
 
 interface RegisterUserCaseRequest {
@@ -8,23 +8,16 @@ interface RegisterUserCaseRequest {
 }
 
 export class RegisterUseCase {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  constructor(private usersRepository: any) {}
+  constructor(private usersRepository: UsersRepository) {}
 
   async execute({ name, email, password }: RegisterUserCaseRequest) {
     const password_hash = await hash(password, 6)
 
-    const userWithSameEmail = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    })
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
     if (userWithSameEmail) {
       throw new Error('E-mail already exists.')
     }
-
-    // const prismaUsersRepository = new PrismaUsersRepository()
 
     await this.usersRepository.create({ name, email, password_hash })
   }
