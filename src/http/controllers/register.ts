83d5025aced0c7db40e4797054a084203ab1d2c4
Anderson Almeida <andersonpa10pa@gmail.com>
2026-04-04 11:55:45 -0300

    Utilizando Factory Pattern

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,10 +1,11 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import z from 'zod'
-import { RegisterUseCase } from '@/use-cases/resgister'
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
+import { makeRegisterUseCase } from '@/use-cases/factories/make-register-use-case'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerUserCase = makeRegisterUseCase()
+
   const registerBodySchema = z.object({
     name: z.string(),
     email: z.email(),
@@ -14,9 +15,6 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(request.body)
 
   try {
-    const prismaUsersRepository = new PrismaUsersRepository()
-    const registerUserCase = new RegisterUseCase(prismaUsersRepository)
-
     await registerUserCase.execute({
       name,
       email,
