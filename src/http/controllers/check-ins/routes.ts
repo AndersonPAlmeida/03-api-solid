7859a382a955625325a75477fb831c6f3966c050
Anderson Almeida <andersonpa10pa@gmail.com>
2026-05-12 21:44:37 -0300

    Testes E2E de rotas de check-ins

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -9,8 +9,9 @@ export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
   app.get('/check-ins/history', history)
-  app.patch('/check-ins/metrics', metrics)
+  app.get('/check-ins/metrics', metrics)
 
   app.post('/gyms/:gymId/check-ins', create)
+
   app.patch('/check-ins/:checkInId/validate', validate)
 }
